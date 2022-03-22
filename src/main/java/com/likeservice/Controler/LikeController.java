046ccd17d93d4b85bb928d3a1262003c13cfee31
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-22 12:56:37 +0530

    Add get like count Method

--- src/main/java/com/likeservice/Controler/LikeController.java
+++ src/main/java/com/likeservice/Controler/LikeController.java
@@ -2,10 +2,20 @@ package com.likeservice.Controler;
 
 import com.likeservice.Service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LikeController {
     @Autowired
     private LikeService likeService;
+
+    @GetMapping("/postsOrComments/{postOrCommentId}/likes/count")
+    public ResponseEntity<Integer> countLikes(@PathVariable("postOrCommentId") String postOrCommentId){
+        return new ResponseEntity<>(likeService.countLikes(postOrCommentId), HttpStatus.ACCEPTED);
+    }
 }
+
--- src/main/java/com/likeservice/Service/LikeService.java
+++ src/main/java/com/likeservice/Service/LikeService.java
@@ -1,11 +1,26 @@
 package com.likeservice.Service;
 
+import com.likeservice.Model.Like;
 import com.likeservice.Repository.LikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LikeService {
     @Autowired
     private LikeRepository likeRepository;
+
+    public int countLikes(String postOrCommentId){
+        List<Like> allData=likeRepository.findAll();
+        int count=0;
+        for(Like like:allData){
+            if(like.getPostOrCommentId().equals(postOrCommentId)){
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
