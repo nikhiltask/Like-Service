1627ba8001b816ee7421d75fb5eb35641a017dcd
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-22 13:29:35 +0530

    Delete Method

--- src/main/java/com/likeservice/Controler/LikeController.java
+++ src/main/java/com/likeservice/Controler/LikeController.java
@@ -2,10 +2,19 @@ package com.likeservice.Controler;
 
 import com.likeservice.Service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LikeController {
     @Autowired
     private LikeService likeService;
+
+    @DeleteMapping("/postsOrComments/{postOrCommentId}/likes/{likeId}")
+    public ResponseEntity<String> deleteLikeID(@PathVariable("likeId") String likeId, @PathVariable("postOrCommentId") String postOrCommentId ){
+        return new ResponseEntity<>(likeService.deleteLikeID(likeId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/likeservice/Service/LikeService.java
+++ src/main/java/com/likeservice/Service/LikeService.java
@@ -8,4 +8,9 @@ import org.springframework.stereotype.Service;
 public class LikeService {
     @Autowired
     private LikeRepository likeRepository;
+
+    public String deleteLikeID(String likeId){
+        likeRepository.deleteById(likeId);
+        return "Deleted  "+likeId+" successfully";
+    }
 }
